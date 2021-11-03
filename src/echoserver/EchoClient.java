7e01d09a8bf631edb94a1d3927eb9bc350a25dd5
crame160 <dylancramer@protonmail.com>
2021-11-02 19:18:15 -0500

    Cleaned up some now defunct files. Started work on the implementation of the EchoClient that should theoretically function but hangs when it runs. Created stress rest bash script.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,17 +7,80 @@ import java.net.Socket;
 
 public class EchoClient extends Throwable {
 	public static final int PORT_NUMBER = 6013;
+	public static InputStream socketInputStream = null;
+	public static OutputStream socketOutputStream = null;
+	public static Socket socket = null;
+
 
 	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
+		String hostName;
+
+		if (args.length == 0) {
+			hostName = "127.0.0.1";
+		} else {
+			hostName = args[0];
+		}
+
+
+		try {
+			socket = new Socket(hostName, PORT_NUMBER);
+			socketInputStream = socket.getInputStream();
+			socketOutputStream = socket.getOutputStream();
+
+			System.out.println("thread");
+
+			Thread outputWriter = new Thread(new outputWriter());
+
+
+			Thread inputReader = new Thread(new inputReader());
+
+			System.out.println("start");
+			outputWriter.start();
+			inputReader.start();
+
+			System.out.println("join");
+			outputWriter.join();
+			inputReader.join();
+
+
+
+			socket.close();
+
+			System.out.println("close");
+		} catch(Exception e){
+			System.out.println(e);
+		}
+
 	}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-
-		// Put your code here.
+	public static class inputReader implements Runnable {
+		@Override
+		public void run() {
+			int currentByte;
+			try {
+				while ((currentByte = System.in.read()) != -1) {
+					socketOutputStream.write(currentByte);
+					socketOutputStream.flush();
+				}
+			} catch(Exception e){
+				System.out.println(e);
+			}
+		}
 	}
+
+	public static class outputWriter implements Runnable {
+		@Override
+		public void run() {
+			int currentByte;
+			try {
+				while ((currentByte = socketInputStream.read()) != -1) {
+					System.out.write(currentByte);
+					System.out.flush();
+				}
+			} catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	}
+
 }
